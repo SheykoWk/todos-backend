9fcab6e794362996cfad5789b27dbd588cf70a92
sheyko <sheyko.wk@gmail.com>
2022-05-18 21:47:05 -0500

    add the get an post controllers

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ app.get('/', (req, res) => {
 
 app.use('/todos', todosRouter)
 
-
-/*
-leer todos los todos
-leer un todo en especifico
-crear un todo
-eliminar un todo
-editar un todo
-*/
-
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT} `)
 })
--- router/todos.js
+++ router/todos.js
@@ -1,17 +1,54 @@
 const router = require('express').Router()
 
+const todosDB = [
+    {
+        id: 1,
+        title: "Primer todo",
+        description: "ndslkfjanskdbf",
+        isCompleted: false
+    }
+]
+
 router.get('/',(req, res) => {
-    res.send('<h1>Obteniendo todos los todos</h1>')
+    res.json(todosDB)
 })
+
 router.get('/:id',(req, res) => {
-    res.send('<h1>Obteniendo un todo en especifico</h1>')
+
+    const id = Number(req.params.id)
+
+    const data = todosDB.filter(item => item.id === id )
+
+    if(data.length){
+        res.json(data)
+    } else {
+        res.status(418).json({
+            message: "Try with another id, this todo is undefined"
+        })
+    }
+
 })
+
 router.post('/',(req, res) => {
-    res.send('<h1>Creando un todo</h1>')
+
+    const data = req.body
+
+    const newObj = {
+        id: todosDB[todosDB.length - 1].id + 1,
+        title: data.title,
+        description: data.description,
+        isCompleted: false
+    }
+
+    todosDB.push(newObj)
+
+    res.status(201).json(todosDB)
 })
+
 router.delete('/:id',(req, res) => {
     res.send('<h1>Eliminiando un todo</h1>')
 })
+
 router.put('/:id',(req, res) => {
     res.send('<h1>Editando un todo</h1>')
 })
